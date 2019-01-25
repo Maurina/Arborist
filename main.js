347cae8c92dd2ac446bad755914fd56055f5394f
Maurina Davis <Maurina.Davis@gmail.com>
2019-01-25 11:26:08 -0700

    sort function added

--- main.js
+++ main.js
@@ -37,28 +37,62 @@ addPear.onclick = addPearTree;
 /*---------------- Removing items from the array -------------------*/
 
 let removefirstTree = function(){
+    if(treesArray.length<1){
+        document.getElementById('errorMessages').innerHTML = "Your tree list is empty."
+    }else{
     treesArray.shift();
     displayTreeArray();
+    }
 };
 
 let removefirst = document.getElementById('removeTreeOne');
 removefirst.onclick = removefirstTree;
 
 let removeSecondTree = function(){
+    if(treesArray.length<2){
+        document.getElementById('errorMessages').innerHTML = "Your tree list does not have a second item."
+    }else{
     treesArray.splice(1,1);
     displayTreeArray();
+    }
 };
 
 let removeSecond = document.getElementById('removeTreeTwo');
 removeSecond.onclick = removeSecondTree;
 
 let removeLastTree = function(){
+    if(treesArray.length<1){
+        document.getElementById('errorMessages').innerHTML = "Your tree list is empty."
+    }else{
     treesArray.pop();
     displayTreeArray();
+    }
 };
 
 let removeLast = document.getElementById('removeLast');
 removeLast.onclick = removeLastTree;
+
+/*---------------- Sort Array ------------------------------------ */
+let sortABCOrder = function(){
+    treesArray.sort();
+    displayTreeArray();
+};
+
+let sortAlpha = document.getElementById('SortByLetter');
+sortAlpha.onclick = sortABCOrder
+
+let makeLowerCase = function(){
+    let treesmall = "";
+    let x;
+    for ( let x = 0; x<treesArray.length; x++){
+        treesArray[x] = treesArray[x].toLowerCase();
+        treesmall += treesArray[x] + "<br>";
+    }
+    document.getElementById('treeList').innerHTML = treesmall;
+    };//displayTreeArray
+
+let lowerCase = document.getElementById('MakeLowerCase');
+lowerCase.onclick = makeLowerCase;
 /*--------------- Asking for specific items in the array -------------------*/
 
 let displayItem3 = function(){
